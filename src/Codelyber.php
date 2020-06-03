c09672993a78a8f1a8b775ee9113aefa98699fc6
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-03 16:49:19 +0200

    initial prove of bitly api

--- src/Codelyber.php
+++ src/Codelyber.php
@@ -2,14 +2,71 @@
 
 declare(strict_types = 1);
 
+
+require_once "../vendor/autoload.php";
+
+use GuzzleHttp\Client;
+
+$client = new Client([
+    // Base URI is used with relative requests
+    'base_uri' => 'https://api-ssl.bitly.com/',
+]);
+
+$response = $client->request('POST', 'v4/bitlinks', [
+    'json' => [
+        'long_url' => 'https://www.youtube.com/watch?v=sWoIUeZzCtM',
+    ],
+    'headers' => [
+        'Authorization' => 'ca42b938cdf1eb06faafe7dc0222853d12a4456d'
+    ],
+    'verify' => false,
+]);
+
+if(in_array($response->getStatusCode(), [200, 201])) {
+    $body = $response->getBody()->getContents();
+    $arr_body = json_decode($body);
+    echo $arr_body->link . "\n";
+}
+
+
+
+/*require_once '../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client();
+
+$long_url = 'https://www.youtube.com/watch?v=sWoIUeZzCtM';
+
+
+$response = $client->post(
+    'https://api-ssl.bitly.com/v4/shorten', [
+        'headers' => [
+            'Content-type' => 'application/json; charset=utf-8',
+            'Authorization' => 'Bearer ' . 'ca42b938cdf1eb06faafe7dc0222853d12a4456d',
+            'Accept'        => 'application/json'
+        ],
+        'json' => ['long_url' => $long_url]
+    ]
+);
+
+if(in_array($response->getStatusCode(), [200, 201])) {
+    $body = $response->getBody()->getContents();
+    //var_dump($body);
+     $arr_body = json_decode($body);
+     echo $arr_body->link. "\n";
+}
+*/
+
+/*
 namespace CodelyTv\PhpBootstrap;
 
 final class Codelyber
 {
-    /** string */
+
     private const GREETING = "CodelyTV";
 
-    /** @var string */
+
     private $name;
 
     public function __construct(string $aName)
@@ -27,3 +84,4 @@ final class Codelyber
         return self::GREETING;
     }
 }
+*/
