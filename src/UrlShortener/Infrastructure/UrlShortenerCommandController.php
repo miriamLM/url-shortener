65e9ece05ff0c28a68a27c2b91e50e6afd73405b
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-06 17:15:38 +0200

    create a controller class for the entry url

---
+++ src/UrlShortener/Infrastructure/UrlShortenerCommandController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaSalle\UrlShortener\MiriamLopez\UrlShortener\Infrastructure;
+
+final class UrlShortenerCommandController
+{
+    public function __construct()
+    {
+    }
+
+    public function __invoke()
+    {
+    }
+}
