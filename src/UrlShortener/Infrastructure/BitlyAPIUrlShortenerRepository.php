c5d49c02fb4f5e1e8d6314fe45c3d454de43560c
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-06 16:59:38 +0200

    create class which implements the method of UrlShortenerRepository

---
+++ src/UrlShortener/Infrastructure/BitlyAPIUrlShortenerRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaSalle\UrlShortener\MiriamLopez\UrlShortener\Infrastructure;
+
+use LaSalle\UrlShortener\MiriamLopez\UrlShortener\Domain\UrlShortenerRepository;
+
+final class BitlyAPIUrlShortenerRepository implements UrlShortenerRepository
+{
+    public function urlShorten()
+    {
+    }
+}
