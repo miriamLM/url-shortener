aadea82a0dddc4d01f9484c8cd7446d2a7f33f4f
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-08 23:14:41 +0200

    update method implementation of the UrlShortenerRepository with the change of the type entry and update de exceptions for the ones created which are more specific

--- src/UrlShortener/Infrastructure/BitlyAPIUrlShortenerRepository.php
+++ src/UrlShortener/Infrastructure/BitlyAPIUrlShortenerRepository.php
@@ -4,15 +4,17 @@ declare(strict_types=1);
 
 namespace LaSalle\UrlShortener\MiriamLopez\UrlShortener\Infrastructure;
 
-use Exception;
 use GuzzleHttp\Exception\ClientException;
+use LaSalle\UrlShortener\MiriamLopez\UrlShortener\Domain\Exceptions\UrlNotFound;
+use LaSalle\UrlShortener\MiriamLopez\UrlShortener\Domain\UrlName;
 use LaSalle\UrlShortener\MiriamLopez\UrlShortener\Domain\UrlShortenerRepository;
 use GuzzleHttp\Client;
+use LaSalle\UrlShortener\MiriamLopez\UrlShortener\Infrastructure\Exceptions\BitlyAPIUnavailable;
 
 final class BitlyAPIUrlShortenerRepository implements UrlShortenerRepository
 {
 
-    public function urlShorten(string $entryUrl): string
+    public function urlShorten(UrlName $url): string
     {
         $client = new Client(
             [
@@ -26,7 +28,7 @@ final class BitlyAPIUrlShortenerRepository implements UrlShortenerRepository
                 'v4/bitlinks',
                 [
                     'json' => [
-                        'long_url' => $entryUrl,
+                        'long_url' => $url->value(),
                     ],
                     'headers' => [
                         'Authorization' => 'ca42b938cdf1eb06faafe7dc0222853d12a4456d'
@@ -35,14 +37,18 @@ final class BitlyAPIUrlShortenerRepository implements UrlShortenerRepository
                 ]
             );
         } catch (ClientException $exception) {
-            throw new Exception();
+            throw new UrlNotFound();
         }
 
         $statusResponse = $response->getStatusCode();
         $bodyResponse = $response->getBody()->getContents();
 
         if (404 == $statusResponse) {
-            throw new Exception();
+            throw new UrlNotFound();
+        }
+
+        if (200 != $statusResponse) {
+            throw new BitlyAPIUnavailable();
         }
 
         $shortenUrl = json_decode($bodyResponse);
