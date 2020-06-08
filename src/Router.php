297695b63c0ae69e03dd0379c7c6b9f431a489f7
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-08 23:18:41 +0200

    update Router with the exceptions that can be read by humans

--- src/Router.php
+++ src/Router.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use LaSalle\UrlShortener\MiriamLopez\Shared\Infrastructure\Exceptions\ExceptionToHumanMessage;
 use LaSalle\UrlShortener\MiriamLopez\UrlShortener\ApplicationService\UrlShortenerService;
 use LaSalle\UrlShortener\MiriamLopez\UrlShortener\Infrastructure\BitlyAPIUrlShortenerRepository;
 use LaSalle\UrlShortener\MiriamLopez\UrlShortener\Infrastructure\UrlShortenerCommandController;
@@ -17,7 +18,10 @@ if (isset($argv)) {
         $controller = new UrlShortenerCommandController($urlShortenerService);
         echo $controller($url) . "\n";
     } catch (RuntimeException $exception) {
-        throw new Exception();
+        $exceptionToHumanMessage = new ExceptionToHumanMessage();
+        echo $exceptionToHumanMessage->map(get_class($exception)) . '. Error code: ' . $exception->getCode(
+        ) . '.' . "\n";
     }
+
     return;
 }
