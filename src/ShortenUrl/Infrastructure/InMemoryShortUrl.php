6a37507c640427c782624c6210f764378ad370d0
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-10 23:18:59 +0200

    update InMemoryShortUrl class passing the connection db and the implementation of the bitly url shortener for constructor

--- src/ShortenUrl/Infrastructure/InMemoryShortUrl.php
+++ src/ShortenUrl/Infrastructure/InMemoryShortUrl.php
@@ -4,11 +4,23 @@ declare(strict_types=1);
 
 namespace LaSalle\UrlShortener\MiriamLopez\ShortenUrl\Infrastructure;
 
+use LaSalle\UrlShortener\MiriamLopez\Shared\Infrastructure\UrlShortenerDBConnection;
 use LaSalle\UrlShortener\MiriamLopez\ShortenUrl\Domain\UrlName;
 use LaSalle\UrlShortener\MiriamLopez\ShortenUrl\Domain\UrlShortenerRepository;
 
 final class InMemoryShortUrl implements UrlShortenerRepository
 {
+    private UrlShortenerDBConnection $connectionDB;
+    private BitlyAPIUrlShortenerRepository $bitlyAPIUrlShortenerRepository;
+
+    public function __construct(
+        UrlShortenerDBConnection $connectionDB,
+        BitlyAPIUrlShortenerRepository $bitlyAPIUrlShortenerRepository
+    ) {
+        $this->connectionDB = $connectionDB;
+        $this->bitlyAPIUrlShortenerRepository = $bitlyAPIUrlShortenerRepository;
+    }
+
 
     public function urlShorten(UrlName $url): string
     {
