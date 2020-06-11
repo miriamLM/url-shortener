f7a878b12cef50c765d6e8fa87d9f493dc8e7039
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-12 01:46:34 +0200

    pass for constructor the connection to DB and add methods to be implemented

--- src/UrlCounter/Infrastructure/InMemoryUrlCounter.php
+++ src/UrlCounter/Infrastructure/InMemoryUrlCounter.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace LaSalle\UrlShortener\MiriamLopez\UrlCounter\Infrastructure;
 
+use LaSalle\UrlShortener\MiriamLopez\Shared\Infrastructure\UrlShortenerDBConnection;
 use LaSalle\UrlShortener\MiriamLopez\UrlCounter\Domain\UrlCounterRepository;
+use LaSalle\UrlShortener\MiriamLopez\UrlCounter\Domain\UtmCampaign;
+use LaSalle\UrlShortener\MiriamLopez\UrlCounter\Domain\UtmCampaignCounter;
 
 final class InMemoryUrlCounter implements UrlCounterRepository
 {
+    private UrlShortenerDBConnection $connectionDB;
 
+    public function __construct(UrlShortenerDBConnection $connectionDB)
+    {
+        $this->connectionDB = $connectionDB;
+    }
+
+    public function findByUtmCampaign(UtmCampaign $utmCampaign): ?UtmCampaignCounter
+    {
+    }
+
+    public function save(UtmCampaignCounter $utmCampaignCounter): void
+    {
+    }
 }
