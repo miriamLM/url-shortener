d099dad60005415aea6c943ae9e8621d030288bd
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-11 23:52:42 +0200

    pass for constructor the interface for future call of method implemented to increase counter of url shorten

--- src/UrlCounter/ApplicationService/IncreaseUrlCounterListener.php
+++ src/UrlCounter/ApplicationService/IncreaseUrlCounterListener.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace LaSalle\UrlShortener\MiriamLopez\UrlCounter\ApplicationService;
 
 use LaSalle\UrlShortener\MiriamLopez\Shared\Domain\UrlNameCreatedDomainEvent;
+use LaSalle\UrlShortener\MiriamLopez\UrlCounter\Domain\UrlCounterRepository;
 
 final class IncreaseUrlCounterListener
 {
+    private UrlCounterRepository $urlCounterRepository;
 
-    public function __construct()
+    public function __construct(UrlCounterRepository $urlCounterRepository)
     {
+        $this->urlCounterRepository = $urlCounterRepository;
     }
 
     public function increaseUrlCounter(UrlNameCreatedDomainEvent $event)
