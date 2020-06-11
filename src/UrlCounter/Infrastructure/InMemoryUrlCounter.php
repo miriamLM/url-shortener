4e477d4891511146349930ac4037601509ac17b8
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-11 23:51:51 +0200

    create implementation of interface for increase the count of url shorten

---
+++ src/UrlCounter/Infrastructure/InMemoryUrlCounter.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaSalle\UrlShortener\MiriamLopez\UrlCounter\Infrastructure;
+
+use LaSalle\UrlShortener\MiriamLopez\UrlCounter\Domain\UrlCounterRepository;
+
+final class InMemoryUrlCounter implements UrlCounterRepository
+{
+
+}
