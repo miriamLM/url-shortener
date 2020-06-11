1fe9270b335e38b1761ad18c292b10a116684071
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-12 01:53:15 +0200

    update if the utm campaign was not found, I create it with the counter at 0

--- src/UrlCounter/ApplicationService/IncreaseUrlCounterListener.php
+++ src/UrlCounter/ApplicationService/IncreaseUrlCounterListener.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace LaSalle\UrlShortener\MiriamLopez\UrlCounter\ApplicationService;
 
 use LaSalle\UrlShortener\MiriamLopez\Shared\Domain\UrlNameCreatedDomainEvent;
+use LaSalle\UrlShortener\MiriamLopez\UrlCounter\Domain\Counter;
 use LaSalle\UrlShortener\MiriamLopez\UrlCounter\Domain\UrlCounterRepository;
 use LaSalle\UrlShortener\MiriamLopez\UrlCounter\Domain\UtmCampaign;
+use LaSalle\UrlShortener\MiriamLopez\UrlCounter\Domain\UtmCampaignCounter;
 
 final class IncreaseUrlCounterListener
 {
@@ -22,5 +24,8 @@ final class IncreaseUrlCounterListener
         $utmCampign = new UtmCampaign($event->utmCampaignValue());
         $utmCampaignCounter = $this->urlCounterRepository->findByUtmCampaign($utmCampign);
 
+        if (null === $utmCampaignCounter) {
+            $utmCampaignCounter = new UtmCampaignCounter($utmCampign, new Counter(0));
+        }
     }
 }
