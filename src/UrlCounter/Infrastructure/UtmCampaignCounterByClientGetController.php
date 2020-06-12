00b16220f356aa69e9ef84d2b1f556004732b08b
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-12 03:38:40 +0200

    create controller class for http get access

---
+++ src/UrlCounter/Infrastructure/UtmCampaignCounterByClientGetController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaSalle\UrlShortener\MiriamLopez\UrlCounter\Infrastructure;
+
+final class UtmCampaignCounterByClientGetController
+{
+    public function __construct()
+    {
+    }
+}
