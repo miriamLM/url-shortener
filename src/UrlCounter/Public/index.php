14efab0a357360ec0331237d21ff6421ac94d819
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-06-13 19:13:31 +0200

    delete shared instance

--- src/UrlCounter/Public/index.php
+++ src/UrlCounter/Public/index.php
@@ -2,12 +2,9 @@
 
 use LaSalle\UrlShortener\MiriamLopez\ShortenUrl\Infrastructure\SymfonyEventDispatcher;
 use LaSalle\UrlShortener\MiriamLopez\UrlCounter\ApplicationService\IncreaseUrlCounterListener;
-use LaSalle\UrlShortener\MiriamLopez\UrlCounter\Infrastructure\InMemoryUrlCounter;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 $dispatcher = new EventDispatcher();
-
-$inMemoryUrlCounter = new InMemoryUrlCounter($connectionDB);
 
 $eventDispatcher = new SymfonyEventDispatcher($dispatcher);
 
